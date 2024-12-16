9565100ad3955abd2e1cd8429a76ac1e653542a2
Pavol Bezek <bezek.palino@gmail.com>
2024-12-16 17:21:43 +0100

    change entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ done
 docker compose -f ňdocker-compose.yml up -d --build
 docker exec -it laravel-app-prod bash
 php artisan migrate --force
-#php artisan serve --host=0.0.0.0 --port=8000
+php artisan serve --host=0.0.0.0 --port=8000
